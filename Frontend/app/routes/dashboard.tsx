97143775409ef85c14f30809d94a98a65f77ddbe
Tomingremeau <tomingremeaumaillet@gmail.com>
2026-05-11 13:46:42 +0200

    modification du dashboard car le render ce fait avant le chargement des données donc le refrsh de page crée  un problème profile undefined

--- Frontend/app/routes/dashboard.tsx
+++ Frontend/app/routes/dashboard.tsx
@@ -1,25 +1,46 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
 import { useAuth } from "../context/AuthContext";
 import { getUserInfo } from "../services/api";
 
 export default function Dashboard() {
   const { token } = useAuth();
+  const navigate = useNavigate();
+
   const [user, setUser] = useState<any>(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    if (!token) return;
+
     async function load() {
-      const data = await getUserInfo(token);
-      setUser(data);
+      try {
+        const data = await getUserInfo(token);
+        setUser(data);
+      } catch (error) {
+        navigate("/login");
+      } finally {
+        setLoading(false);
+      }
     }
 
     load();
-  }, []);
+  }, [token]);
 
-  if (!user) return <p>Chargement</p>;
+  // on attend de recevoir le token
+  if (!token) return <p>Chargement session...</p>;
+
+  // attend API
+  if (loading) return <p>Chargement dashboard...</p>;
+
+  // sécurité
+  if (!user || !user.profile) return <p>Aucune donnée</p>;
 
   return (
     <main>
       <h1>Bonjour {user.profile.firstName}</h1>
+
+      <p>Nom : {user.profile.lastName}</p>
       <p>Distance : {user.statistics.totalDistance} km</p>
     </main>
   );
