d3462affe17bdf26f37274d919c87efae76071b6
Tomingremeau <tomingremeaumaillet@gmail.com>
2026-05-12 17:17:10 +0200

    ajout du graphique bpm

--- Frontend/app/routes/dashboard.tsx
+++ Frontend/app/routes/dashboard.tsx
@@ -3,11 +3,9 @@ import { useNavigate } from "react-router";
 import { useAuth } from "../context/AuthContext";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import {
-  getUserInfo,
-  getUserActivity,
-} from "../services/api";
+import { getUserInfo, getUserActivity, } from "../services/api";
 import "../css/Dashboard.css";
+import { ResponsiveContainer, ComposedChart, Bar, Line, XAxis, YAxis, Tooltip, CartesianGrid, Legend, } from "recharts";
 
 export default function Dashboard() {
   /* déclaration des states */
@@ -16,6 +14,8 @@ export default function Dashboard() {
   const [user, setUser] = useState<any>(null);
   const [activities, setActivities] = useState<any[]>([]);
   const [loading, setLoading] = useState(true);
+  /* semaine active */
+  const [currentWeek, setCurrentWeek] = useState(0);
 
   useEffect(() => {
     if (!token) return;
@@ -64,6 +64,87 @@ export default function Dashboard() {
       </div>
     );
   }
+  /* données pour les graphiques */
+
+  /* découpage des activités */
+  const activitiesPerWeek = 7;
+
+ /* tri des activités par date */
+const sortedActivities = [...activities].sort(
+  (a, b) =>
+    new Date(a.date).getTime() -
+    new Date(b.date).getTime()
+);
+
+/* découpage par semaine */
+const groupedActivities = [];
+
+for (
+  let i = 0;
+  i < sortedActivities.length;
+  i += activitiesPerWeek
+) {
+  groupedActivities.push(
+    sortedActivities.slice(
+      i,
+      i + activitiesPerWeek
+    )
+  );
+}
+
+  /* activités affichées */
+  const currentActivities =
+    groupedActivities[currentWeek] || [];
+
+  /* données graphiques */
+  const heartRateData = currentActivities.map(
+    (activity: any) => ({
+      day: new Date(activity.date)
+        .toLocaleDateString("fr-FR", {
+          weekday: "short",
+        })
+        .replace(".", ""),
+      min: activity.heartRate.min,
+      max: activity.heartRate.max,
+      average: activity.heartRate.average,
+      fullDate: activity.date,
+    })
+  );
+
+  /* bpm moyen */
+  const averageHeartRate =
+    heartRateData.length > 0
+      ? Math.round(
+        heartRateData.reduce(
+          (acc, item) =>
+            acc + item.average,
+          0
+        ) / heartRateData.length
+      )
+      : 0;
+
+  /* période affichée */
+const firstDate = currentActivities[0]?.date;
+
+const lastDate =
+  currentActivities[
+    currentActivities.length - 1
+  ]?.date;
+
+const formattedPeriod =
+  firstDate && lastDate
+    ? `Période du ${new Date(
+        firstDate
+      ).toLocaleDateString("fr-FR", {
+        day: "numeric",
+        month: "long",
+      })} au ${new Date(
+        lastDate
+      ).toLocaleDateString("fr-FR", {
+        day: "numeric",
+        month: "long",
+      })}`
+    : "";
 
   return (
 
@@ -73,9 +154,7 @@ export default function Dashboard() {
 
       <section className="dashboard-content">
 
-        {/* =========================
-            HEADER
-        ========================= */}
+        {/* HEADER */}
 
         <div className="dashboard-header-card">
 
@@ -119,17 +198,13 @@ export default function Dashboard() {
           </div>
         </div>
 
-        {/* =========================
-            TITLE
-        ========================= */}
+        {/* TITLE */}
 
         <h1 className="dashboard-section-title">
           Vos dernières performances
         </h1>
 
-        {/* =========================
-            STATS
-        ========================= */}
+        {/* STATS*/}
 
         <div className="dashboard-stats-grid">
 
@@ -163,7 +238,7 @@ export default function Dashboard() {
             <div className="dashboard-stat-top">
               <div className="dashboard-stat-title dashboard-stat-title-red">
                 <h2>
-                  163 BPM
+                  {averageHeartRate} BPM
                 </h2>
                 <p>
                   Fréquence cardiaque moyenne
@@ -171,22 +246,110 @@ export default function Dashboard() {
               </div>
 
               <div className="dashboard-period">
-                <button>{"<"}</button>
+                <button
+                  onClick={() =>
+                    setCurrentWeek((prev) =>
+                      prev > 0 ? prev - 1 : prev
+                    )
+                  }
+                >
+                  {"<"}
+                </button>
                 <span>
-                  28 mai - 04 juin
+                  {formattedPeriod}
                 </span>
-                <button>{">"}</button>
+                <button
+                  onClick={() =>
+                    setCurrentWeek((prev) =>
+                      prev < groupedActivities.length - 1
+                        ? prev + 1
+                        : prev
+                    )
+                  }
+                >
+                  {">"}
+                </button>
               </div>
+            </div>
+
+            <div className="dashboard-heart-chart">
+              <ResponsiveContainer
+                width="100%"
+                height={320}
+              >
+
+                <ComposedChart data={heartRateData}>
+
+                  <CartesianGrid
+                    strokeDasharray="3 3"
+                    vertical={false}
+                    stroke="#efefef"
+                  />
+
+                  <XAxis
+                    dataKey="day"
+                    tickLine={false}
+                    axisLine={false}
+                    tick={{
+                      fill: "#777",
+                      fontSize: 14,
+                    }}
+                  />
+
+                  <YAxis
+                    domain={[130, 187]}
+                    tickLine={false}
+                    axisLine={false}
+                    tick={{
+                      fill: "#777",
+                      fontSize: 13,
+                    }}
+                  />
+
+                  <Tooltip />
+
+                  <Legend />
+
+                  {/* BPM MIN */}
+                  <Bar
+                    dataKey="min"
+                    name="MinBPM"
+                    fill="#ffd2ca"
+                    radius={[10, 10, 0, 0]}
+                    barSize={14}
+                  />
+
+                  {/* BPM MAX */}
+                  <Bar
+                    dataKey="max"
+                    name="Max BPM"
+                    fill="#ff3b13"
+                    radius={[10, 10, 0, 0]}
+                    barSize={14}
+                  />
+
+                  {/* BPM MOYEN */}
+                  <Line
+                    type="monotone"
+                    dataKey="average"
+                    name="BPM Moyen"
+                    stroke="#2b46ff"
+                    strokeWidth={3}
+                    dot={{
+                      r: 4,
+                      fill: "#2b46ff",
+                    }}
+                  />
+
+                </ComposedChart>
+              </ResponsiveContainer>
 
             </div>
-            {/* chart fake */}
-            <div className="dashboard-chart-placeholder" />
+
           </div>
         </div>
 
-        {/* =========================
-            ACTIVITÉS
-        ========================= */}
+        {/* ACTIVITÉS*/}
 
         <section className="dashboard-activities">
           <h1 className="dashboard-section-title">
