7f3e18057e7df287b95df43fcd1f009c11d303b7
Tomingremeau <tomingremeaumaillet@gmail.com>
2026-05-12 13:45:11 +0200

    design login + animation logo

--- Frontend/app/routes/login.tsx
+++ Frontend/app/routes/login.tsx
@@ -3,6 +3,10 @@ import { useNavigate } from "react-router";
 import { loginUser } from "../services/api";
 import { useAuth } from "../context/AuthContext";
 
+import "../css/Login.css";
+
+import BannerLogin from "../../assets/Bannierelogin.png";
+
 export default function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -10,7 +14,7 @@ export default function Login() {
   const { login } = useAuth();
   const navigate = useNavigate();
 
-  async function handleSubmit(e:any) {
+  async function handleSubmit(e: any) {
     e.preventDefault();
 
     const data = await loginUser(username, password);
@@ -20,23 +24,58 @@ export default function Login() {
   }
 
   return (
-    <main>
-      <h1>Connexion</h1>
+    <main className="login-page">
+      <section className="login-left">
+        <div className="login-logo">
+  <div className="logo-bars">
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+  </div>
 
-      <form onSubmit={handleSubmit}>
-        <input
-          placeholder="username"
-          onChange={(e) => setUsername(e.target.value)}
-        />
+  <span className="logo-text">SPORTSEE</span>
+</div>
 
-        <input
-          type="password"
-          placeholder="password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
+        <div className="login-card">
+          <h1 className="login-title">
+            Transformez
+            <br />
+            vos stats en résultats
+          </h1>
 
-        <button>Se connecter</button>
-      </form>
+          <h2 className="login-subtitle">Se connecter</h2>
+
+          <form className="login-form" onSubmit={handleSubmit}>
+            <label>Adresse email</label>
+
+            <input
+              type="text"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+
+            <label>Mot de passe</label>
+
+            <input
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <button type="submit">Se connecter</button>
+          </form>
+
+          <p className="forgot-password">Mot de passe oublié ?</p>
+        </div>
+      </section>
+
+      <section className="login-right">
+        <img src={BannerLogin} alt="Sportsee login banner" />
+
+        <div className="login-bubble">
+          Analysez vos performances en un clin d'œil,
+          suivez vos progrès et atteignez vos objectifs.
+        </div>
+      </section>
     </main>
   );
 }
